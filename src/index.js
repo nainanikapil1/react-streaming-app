f8442cacf55e771e594dd5a2a48709c6b212db33
nainanikapil1 <nainanikapil1@gmail.com>
2018-03-03 12:08:45 +0530

    set stage with api call

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyBZJr23idl22iOKn3goHkh0jBsQfMV2DZ0';
 
-const App = () => {
-  return (<div>
-    <SearchBar />
-  </div>);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state({ videos: [] })
+    YTSearch({ key: API_KEY, term: 'oranges' }, (videos) => {
+      this.setState({ videos: videos })
+    });
+  }
+  render() {
+    return (<div>
+      <SearchBar />
+    </div>);
+  }
 }
+
 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
