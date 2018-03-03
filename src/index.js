4d0b60ace1540ff1481e02f639b5ba46e0c608e1
Kapil Nainani <nainani>
2018-03-03 11:27:18 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyBZJr23idl22iOKn3goHkh0jBsQfMV2DZ0';
 
-import App from './components/app';
-import reducers from './reducers';
+const App = () => {
+  return (<div>
+    <SearchBar />
+  </div>);
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
