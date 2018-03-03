4711f7f0572d4dba343eb7b2ee52960ccbf78796
nainanikapil1 <nainanikapil1@gmail.com>
2018-03-03 16:14:53 +0530

    videos rendering

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBZJr23idl22iOKn3goHkh0jBsQfMV2DZ0';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state({ videos: [] })
+    this.state = { videos: [] };
     YTSearch({ key: API_KEY, term: 'oranges' }, (videos) => {
-      this.setState({ videos: videos })
+      this.setState({ videos })
     });
   }
   render() {
     return (<div>
       <SearchBar />
+      <VideoList videos={this.state.videos} />
     </div>);
   }
 }
